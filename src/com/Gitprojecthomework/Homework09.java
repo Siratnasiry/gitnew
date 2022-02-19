5543d458454876540fa110d737687d924609d18b
Zohra Faizi <zohrafaizi966@gmail.com>
2022-02-18 20:54:50 -0500

    2nd commit

--- src/com/Gitprojecthomework/Homework09.java
+++ src/com/Gitprojecthomework/Homework09.java
@@ -5,7 +5,7 @@ public class Homework09 {
 	public static void main(String[] args) {
 		// Write a java program to find the second largest number in the array
 		
-		int[] number = { 10, 20, 30, 40, 50 };
+		int[] number = { 20, 40, 30, 100, 50 };
 		int largest = number[0];
 		int SecLargest = number[0];
 		for (int n = 1; n < number.length; n++) {
